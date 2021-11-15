b15437b8bc5df8c32948b158b87d3056a82c4bec
pauLiou <paulozfisher@gmail.com>
2021-11-15 20:06:08 +0100

    test level 1

--- MikeProject/MarsRover.cpp
+++ MikeProject/MarsRover.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <array>
 
+using namespace std;
+
+class Rover {
+public:
+	int currentLocX; //current coordinate x
+	int currentLocY; //current coordinate y
+	char direction; //direction either left or right
+	bool move; // move or not
+};
+
+
+int rectangle(int sizeX,const int sizeY) { // creating the grid for the rover (dynamic array)
+	
+	int** grid = new int* [sizeY];
+	for (int i = 0; i < sizeY; ++i) { // intialise the size of the matrix grid 
+		grid[i] = new int[sizeX];
+	}
+
+	for (int i = 0; i < sizeY; ++i) {
+		for (int k = 0; k < sizeX; ++k) // fill in the coordinates grid
+			grid[sizeX][sizeY] = i;
+	}
+	
+	return grid[1][3];
+	
+	}
 
 
 int main() {
 
+	cout << rectangle(3,5) << endl;
+	
 }
